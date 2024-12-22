a7149efccb55ed384c272fd5e43574059ca30142
Phạm Khắc Duy <137195320+pkduy2006@users.noreply.github.com>
2024-12-23 01:25:10 +0200

    Add files via upload

---
+++ hanhtrinhkhongdung.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long w, l, x;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> w >> l >> x;
+
+    long long numerator = x * (w + l) - 100 * w;
+    long long denominator = 100 - x;
+    if (numerator <= 0) 
+        cout << 0 << '\n';
+    else
+        cout << (numerator + denominator - 1) / denominator << '\n';
+
+    return 0;
+}
